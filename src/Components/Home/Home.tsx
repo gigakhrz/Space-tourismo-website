33e6b067f19b7b206128f19a4167fc989deab015
gigakhrz <kharazishvilig13@gmail.com>
2023-05-21 14:22:24 +0400

    add home page tablet responsive

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,8 @@
 import { styled } from "styled-components";
 import Header from "../Header/Header";
 import homeBgMobile from "../../../public/assets/home/background-home-mobile.jpg";
+import bgTablet from "../../../public/assets/home/background-home-tablet.jpg";
+import bgDesktop from "../../../public/assets/home/background-home-desktop.jpg";
 
 const Home = (): JSX.Element => {
   return (
@@ -35,6 +37,8 @@ const HomeContainer = styled.div`
   background-size: cover;
   @media screen and (min-width: 768px) {
     padding: 0;
+    gap: 106px;
+    background-image: url(${bgTablet});
   }
 
   .description {
@@ -42,6 +46,11 @@ const HomeContainer = styled.div`
     flex-direction: column;
     gap: 16px;
     align-items: center;
+    @media screen and (min-width: 768px) {
+      gap: 24px;
+      width: 450px;
+      margin-bottom: 50px;
+    }
 
     h3 {
       font-family: Barlow Condensed;
@@ -51,6 +60,11 @@ const HomeContainer = styled.div`
       letter-spacing: 2.7px;
       text-align: center;
       color: #d0d6f9;
+      @media screen and (min-width: 768px) {
+        line-height: 24px;
+        font-size: 20px;
+        letter-spacing: 3.375px;
+      }
     }
 
     h1 {
@@ -60,6 +74,10 @@ const HomeContainer = styled.div`
       line-height: 100px;
       text-align: center;
       color: white;
+      @media screen and (min-width: 768px) {
+        line-height: 150px;
+        font-size: 150px;
+      }
     }
 
     p {
@@ -69,6 +87,10 @@ const HomeContainer = styled.div`
       line-height: 25px;
       text-align: center;
       color: #d0d6f9;
+      @media screen and (min-width: 768px) {
+        line-height: 28px;
+        font-size: 16px;
+      }
     }
   }
 
@@ -89,6 +111,16 @@ const HomeContainer = styled.div`
     line-height: 23px;
     letter-spacing: 1.25px;
     text-align: center;
+
+    @media screen and (min-width: 768px) {
+      width: 242px;
+      height: 242px;
+      line-height: 36.37px;
+      font-size: 32px;
+      letter-spacing: 2;
+      margin-top: 0;
+      margin-bottom: 90px;
+    }
   }
 `;
 
